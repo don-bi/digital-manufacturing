d3310243091b63c0fbcc300e9d0d6dab18eaea26
Donald <donb14229@gmail.com>
2026-02-01 15:39:21 -0500

    updated units

--- square.cpp
+++ square.cpp
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
 	// f << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
     f << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">\n";
     f << std::format(
-        "<rect x=\"3\" y=\"3\" width=\"{}\" height=\"{}\" "
+        "<rect x=\"3\" y=\"3\" width=\"{}mm\" height=\"{}mm\" "
         "style=\"stroke-width:1;stroke:rgb(0,0,0);fill:none\" />\n",
         width, height
     );
